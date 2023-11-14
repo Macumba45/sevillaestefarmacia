7ddbfe0ae6d756d0d03592c96eca8560bebbbc0f
Macumba45 <gonzalolovo@gmail.com>
2023-11-14 22:53:25 +0100

    refactor: generateStaticPaths code

--- src/app/services/[service]/page.tsx
+++ src/app/services/[service]/page.tsx
@@ -1,14 +1,30 @@
 import Page from './services'
 import { Services } from '../../../../types/types'
 import { FC } from 'react'
-import { getServices } from '@/services/service'
 
-export async function generateStaticPaths() {
-    const services = (await getServices()) as Services[]
-    const paths = services!.map(service => ({
-        params: { service: service!.id },
-    }))
-    return paths
+export async function generateStaticParams() {
+    try {
+        const headers = {
+            'Content-Type': 'application/json',
+        }
+        const response = await fetch(
+            `${process.env.CLIENT_URL}/api/services/getService`,
+            {
+                method: 'GET',
+                headers,
+            }
+        )
+        if (response.ok) {
+            const data: Services[] = await response.json()
+            return data
+        } else {
+            console.error('Error al obtener los servicios')
+            throw new Error('Error al obtener los servicios')
+        }
+    } catch (error) {
+        console.error('Error al enviar el objeto:', error)
+        throw error
+    }
 }
 
 interface Props {
