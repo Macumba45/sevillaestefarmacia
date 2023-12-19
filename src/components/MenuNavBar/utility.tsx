f29990eee091f6605b53f301ca5083a7a368664b
Macumba45 <gonzalolovo@gmail.com>
2023-12-20 00:34:33 +0100

    fix filter payed items

--- src/components/MenuNavBar/utility.tsx
+++ src/components/MenuNavBar/utility.tsx
@@ -21,7 +21,7 @@ export const pages = [
                 route: '/services/clo0e0mn50003xy040gwqse36',
             },
             {
-                name: 'SPD',
+                name: 'Organiza medicación',
                 route: '/services/clo0e17d30004xy04cjklg2px',
             },
             {
@@ -86,7 +86,7 @@ export const pagesMobile = [
                 route: '/services/clo0e0mn50003xy040gwqse36',
             },
             {
-                name: 'SPD',
+                name: 'Organiza medicación',
                 route: '/services/clo0e17d30004xy04cjklg2px',
             },
             {
