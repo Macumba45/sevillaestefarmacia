811baa067bacafd18f96e83d3496a9fa295003d6
Macumba45 <gonzalolovo@gmail.com>
2024-02-25 12:12:28 +0100

    fix: instagram openBrowser

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -46,12 +46,6 @@ const Home: FC = () => {
         window.open(whatsappURL)
     }
 
-    useEffect(() => {
-        if (navigator.userAgent.includes('Instagram')) {
-            window.location.href = 'https://www.sevillaestefarmacia.com/'
-        }
-    }, [])
-
     return (
         <div>
             <LayoutNavFooter>
