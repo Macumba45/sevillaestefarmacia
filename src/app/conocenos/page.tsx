241fd9bf589b635380d7ab00b89c586bf7f781f2
Macumba45 <gonzalolovo@gmail.com>
2024-02-26 11:44:33 +0100

    fix: wp number talleres

--- src/app/conocenos/page.tsx
+++ src/app/conocenos/page.tsx
@@ -77,20 +77,29 @@ const Conocenos: FC = () => {
     }
 
     const contactWhatsApp = () => {
+        // const phoneNumber = '+34682734237'
+        // let whatsappURL = ''
+
+        // if (
+        //     /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(
+        //         navigator.userAgent
+        //     )
+        // ) {
+        //     // Es un dispositivo móvil
+        //     whatsappURL = `https://api.whatsapp.com/send?phone=${phoneNumber}`
+        // } else {
+        //     // Es un escritorio
+        //     whatsappURL = `https://web.whatsapp.com/send?phone=${phoneNumber}`
+        // }
+
+        // window.open(whatsappURL)
+
         const phoneNumber = '+34682734237'
         let whatsappURL = ''
-
-        if (
-            /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(
-                navigator.userAgent
-            )
-        ) {
-            // Es un dispositivo móvil
-            whatsappURL = `https://api.whatsapp.com/send?phone=${phoneNumber}`
-        } else {
-            // Es un escritorio
-            whatsappURL = `https://web.whatsapp.com/send?phone=${phoneNumber}`
-        }
+        whatsappURL = `https://wa.me/${phoneNumber}`
+        // if (navigator.userAgent.includes('Instagram')) {
+        //     whatsappURL = `https://wa.me/${phoneNumber}`
+        // }
 
         window.open(whatsappURL)
     }
