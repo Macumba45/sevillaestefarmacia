db4ebdacf04546c82d56cc6231f217dc91c3e0b9
Macumba45 <gonzalolovo@gmail.com>
2024-07-10 13:18:34 +0200

    conocenos new schedule

--- src/app/conocenos/page.tsx
+++ src/app/conocenos/page.tsx
@@ -202,9 +202,9 @@ const Conocenos: FC = () => {
                             <ParrafoServices>
                                 <SpanBold>
                                     Horario: <br /> ·Lunes a viernes de 9:00h a
-                                    21.30h (ininterrumpido) <br /> ·Sábado de
-                                    9:30h a 14:.00h <br /> ·Domingo y festivos
-                                    de 9:30h a 14:.00h
+                                    21.30h (ininterrumpido) <br /> ·Sábados de
+                                    9:30h a 14:.00h <br /> ·Domingos: Cerrado
+                                    (meses julio y agosto)
                                 </SpanBold>
                             </ParrafoServices>
                         </ContainerParrafos>
